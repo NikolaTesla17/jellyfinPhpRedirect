7847c0f09d9e491e26d8cbf14de004cfd27c2329
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-07-27 20:56:16 -0700

    Update log.php

--- log.php
+++ log.php
@@ -18,7 +18,7 @@ $current = file_get_contents($file);
 $current .= "$ip\n";
 file_put_contents($file, $current);
 
-$internalIp = $SERVER['SERVER_ADDR']
+$internalIp = $_SERVER['SERVER_ADDR'];
 header("location: http://$internalIp:8096");
 
 ?> 
